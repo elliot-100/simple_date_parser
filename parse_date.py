21852f4a03856a076cf8758bce7bdb831f92ec93
smallship <user@example.com>
2018-01-20 19:28:47 +0000

    Output a sorted list instead of a set

--- parse_date.py
+++ parse_date.py
@@ -20,37 +20,37 @@ def parse_date(date_input):
                 word_patterns.insert(i, 'nnnn')
         words[i] = int(words[i])
 
-    outputs = []
+    dates = set()
 
     if word_patterns == ['nn', 'nn', 'nnnn']:
         try:
-            outputs.append(datetime.date(words[2], words[0], words[1]))  # mm-dd-yyyy
+            dates.add(datetime.date(words[2], words[0], words[1]))  # mm-dd-yyyy
         except ValueError:
             pass
         try:
-            outputs.append(datetime.date(words[2], words[1], words[0]))  # dd-mm-yyyy
+            dates.add(datetime.date(words[2], words[1], words[0]))  # dd-mm-yyyy
         except ValueError:
             pass
 
     if word_patterns == ['nnnn', 'nn', 'nn']:
         try:
-            outputs.append(datetime.date(words[0], words[1], words[2]))  # yyyy-mm-dd
+            dates.add(datetime.date(words[0], words[1], words[2]))  # yyyy-mm-dd
         except ValueError:
             pass
 
     if word_patterns == ['nn', 'nn', 'nn']:
         for cb in century_bases:
             try:
-                outputs.append(datetime.date(cb + words[2], words[1], words[0]))
+                dates.add(datetime.date(cb + words[2], words[1], words[0]))
                 # dd-mm-yy (pre-1000CE), dd-mm-19yy, dd-mm-20yy
             except ValueError:
                 pass
             try:
-                outputs.append(datetime.date(cb + words[2], words[0], words[1]))
+                dates.add(datetime.date(cb + words[2], words[0], words[1]))
                 # mm-dd-yy (pre-1000CE), mm-dd-19yy, mm-dd-20yy
             except ValueError:
                 pass
 
-    outputs = set(outputs)
+    outputs = sorted(list(dates))
 
     return outputs
