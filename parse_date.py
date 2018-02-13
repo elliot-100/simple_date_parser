756f805e11c8514d31cef4f3ac9f1e94de2acc9a
smallship <user@example.com>
2018-02-13 11:31:09 +0000

    Fully implemented new behaviour with yy_leniency parameter and corresponding unit tests

--- parse_date.py
+++ parse_date.py
@@ -2,12 +2,14 @@
 import re
 import datetime
 
+
 def attempt_to_create_date(date_list, arg1, arg2, arg3):
     try:
         # parse input as mm-dd-yyyy
         date_list.append(datetime.date(arg1, arg2, arg3))
     except ValueError:
         pass
+
 
 def parse_date(date_input, yy_leniency=0):
     """ Attempt to derive one or more valid datetime.date object(s) from an input string representing a single date,
@@ -16,7 +18,7 @@ def parse_date(date_input, yy_leniency=0):
     words = re.split('\W', date_input)[:3]  # first 3 words, separated by any punctuation
     word_patterns = []
     dates = []
-    extended_dates = []
+    dates2 = []
 
     for i in range(len(words)):
         if words[i].isdigit:
@@ -43,37 +45,55 @@ def parse_date(date_input, yy_leniency=0):
 
         # parse input as dd-mm-nnyy
 
+        attempt_to_create_date(dates, words[2], words[1], words[0])
         attempt_to_create_date(dates, words[2] + century - 100, words[1], words[0])
         attempt_to_create_date(dates, words[2] + century, words[1], words[0])
         attempt_to_create_date(dates, words[2] + century + 100, words[1], words[0])
 
-        if yy_leniency <= 0:
-            dates = list(dates)
-            dates.sort(key=lambda d: abs(d - today))
-            if dates:
-                dates = [dates[0]]
+        # dates = list(dates)
+        dates.sort(key=lambda d: abs(d - today))
 
-        dates = list(dates)
-        print('dates' + str(dates))
+        if yy_leniency <= 0:
+            dates = dates[0:1]
+        elif yy_leniency == 1:
+            dates = dates[0:2]
+        elif yy_leniency == 2:
+            dates = dates[0:3]
+        elif yy_leniency == 3:
+            dates = dates[0:4]
+
+        print('dates: ' + str(dates))
 
         if words[0] != words[1]:
             # mm and dd values are distinct
             # parse input as mm-dd-nnyy
 
-            attempt_to_create_date(extended_dates, words[2] + century - 100, words[0], words[1])
-            attempt_to_create_date(extended_dates, words[2] + century, words[0], words[1])
-            attempt_to_create_date(extended_dates, words[2] + century + 100, words[0], words[1])
+            attempt_to_create_date(dates2, words[2], words[0], words[1])
+            attempt_to_create_date(dates2, words[2] + century - 100, words[0], words[1])
+            attempt_to_create_date(dates2, words[2] + century, words[0], words[1])
+            attempt_to_create_date(dates2, words[2] + century + 100, words[0], words[1])
+
+            # extended_dates = list(extended_dates)
+            dates2.sort(key=lambda d: abs(d - today))
 
             if yy_leniency <= 0:
-                extended_dates = list(extended_dates)
-                extended_dates.sort(key=lambda d: abs(d - today))
-                if extended_dates:
-                    extended_dates = [extended_dates[0]]
+                dates2 = dates2[0:1]
+            elif yy_leniency == 1:
+                dates2 = dates2[0:2]
+            elif yy_leniency == 2:
+                dates2 = dates2[0:3]
+            elif yy_leniency == 3:
+                dates2 = dates2[0:4]
 
-                extended_dates = list(extended_dates)
-                print('extended_dates' + str(extended_dates))
+            print('dates2: ' + str(dates2))
 
     else:
         pass  # TODO: consider error handling
 
-    return sorted(dates + extended_dates)
+    print('dates+dates2: ' + str(sorted(dates + dates2)))
+    return sorted(dates + dates2)
+
+
+######
+
+parse_date("13-13-40", 3)
